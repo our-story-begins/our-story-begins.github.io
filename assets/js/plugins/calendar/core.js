6fde644fb5e597a999169d4f637155afce5ef12b
吴小帅 <110596709+our-story-begins@users.noreply.github.com>
2023-04-04 03:16:39 +0800

    upd-230404-0316

--- assets/js/plugins/calendar/core.js
+++ assets/js/plugins/calendar/core.js
@@ -4592,7 +4592,7 @@ Docs & License: https://fullcalendar.io/
             prevYear: 'prev year',
             nextYear: 'next year',
             year: 'year',
-            today: 'today',
+            today: 'Today',
             month: 'month',
             week: 'week',
             day: 'day',
